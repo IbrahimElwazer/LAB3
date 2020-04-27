8d39c923a858cd637d37083cf9b4de95e6d644b9
IbrahimElwazer <ibrahimelwazer123@gmail.com>
2020-04-27 18:23:48 +0300

    external activity

--- app/src/main/java/advanced/java/course/lab3/ExternalActivityOAMK.java
+++ app/src/main/java/advanced/java/course/lab3/ExternalActivityOAMK.java
@@ -1,6 +1,11 @@
 package advanced.java.course.lab3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +16,39 @@ public class ExternalActivityOAMK extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oamk_acitivity);
 
+        Button mapButton = findViewById(R.id.map_button);
+        Button callButton = findViewById(R.id.call_button);
+        final EditText editText = findViewById(R.id.text_input);
+        Button goButton = findViewById(R.id.go_button);
 
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri location = Uri.parse("geo:0,0?q=Kotkantie 1, 90250 Oulu, Finland");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startActivity(mapIntent);
+            }
+        });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri number = Uri.parse("tel:+358206110200");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                startActivity(callIntent);
+            }
+        });
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+
+            String oamksite = editText.getText().toString();
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse(oamksite);
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
 
     }
 
